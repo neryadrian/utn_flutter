b4344c54dbf5f34d776f6468861bf7724f56da84
neryadrian <adrian595.nag@gmail.com>
2024-08-22 20:43:26 -0300

    FlutterFire Auth with emulators

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // TODO for development purposes
+  print('Starting emulators');
+  if (kDebugMode) {
+    try {
+      await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+    } catch (e) {
+      print(e);
+    }
+  }
+  print('Finishing emulators start');
+
   runApp(const MainApp());
 }
 
